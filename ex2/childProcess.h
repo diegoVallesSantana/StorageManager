eeb24c3e08aeae5a5e7102b2a837441328f17d6a
Diego <diegoesteban.vallessantana@student.kuleuven.be>
2025-11-20 11:40:28 +0100

    ex2 adjustments

--- ex2/childProcess.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by diegovs on 11/20/25.
-//
-
-#ifndef SM_CHILDPROCESS_H
-#define SM_CHILDPROCESS_H
-
-#endif //SM_CHILDPROCESS_H
--- ex2/main.c
+++
@@ -1,3 +0,0 @@
-//
-// Created by diegovs on 11/20/25.
-//
